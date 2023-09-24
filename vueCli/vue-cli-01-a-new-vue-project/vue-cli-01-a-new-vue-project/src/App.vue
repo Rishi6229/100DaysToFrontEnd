11e5cde6fe096483c01599b21f1e7f33251ac00f
Rishi6229 <rk6180442@gmail.com>
2023-09-24 11:40:38 +0530

    vue-cli-workflow learnt

---
+++ vueCli/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from '../components/FriendContact.vue';
+export default
+{
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "Rishi",
+          name: "Rishi Jha",
+          phone: "2423423",
+          email: "jsjf@fsjfs.com",
+        },
+        {
+          id: "Mayank",
+          name: "Mayank Jha",
+          phone: "2423499",
+          email: "2ersdf@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ vueCli/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">Show details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data(){
+       return{
+        detailsAreVisible:false,
+        friend:{
+            id: "Rishi",
+          name: "Rishi Jha",
+          phone: "2423423",
+          email: "jsjf@fsjfs.com",
+        }
+       }
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsAreVisible != this.detailsAreVisible;
+        }
+    }
+};
+</script>
